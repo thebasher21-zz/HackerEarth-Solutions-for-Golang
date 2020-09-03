79dcc6138a6a09d97947423b9435a4667882567a
Vihaan Tewari <60540439+thebasher21@users.noreply.github.com>
2020-09-03 14:02:19 +0530

    Create Array Rotation.go
    Given an array, A and number of rotations, K, rotate the elements of the array to the left or the right (depending on whether the value of K is positive or negative) and print out the result.

---
+++ Array Rotation.go
@@ -0,0 +1,41 @@
+package main 
+import (
+    f "fmt"
+    "bufio"
+    "os"
+)
+
+func right_rotate(a []int, N, K int) {
+    r := append(a[(N-K)%N:N], a[:(N-K)%N]...)
+    for i:=0; i<len(r); i++ {
+        f.Printf("%d ", r[i])
+    }
+    f.Println("\n")
+}
+
+func left_rotate(a []int, N, K int) {
+    r:=append(a[-K:N], a[:-K]...)
+    for i:=0; i<len(r); i++ {
+        f.Printf("%d ", r[i])
+    }
+    f.Println("\n")
+}
+
+func main() {
+    in := bufio.NewReader(os.Stdin)
+    mat := make([]int,100000)
+    var T, N, K int
+    f.Fscan(in, &T)
+    for ; T>0; T-- {
+        f.Fscan(in, &N, &K)
+        arr := mat[:N]
+        for i:=0; i<N; i++ {
+            f.Fscan(in, &arr[i])
+        }
+        if K>=0{
+            right_rotate(arr, N, K%N)
+        } else {
+            left_rotate(arr, N, K%N)    
+        }
+    }
+}
